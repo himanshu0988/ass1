1a2b793cc98ed2f58860df60e64b7440bfce7312
himanshu0988 <127866082+himanshu0988@users.noreply.github.com>
2023-04-22 16:29:24 +0530

    Add files via upload

---
+++ fifthquestion.java
@@ -0,0 +1,36 @@
+package firstproject;
+
+public class fifthquestion {
+
+	public static void main(String[] args) {
+		{
+			int n = 29;
+			{
+				{
+					for(int i=0;i<n;i++)
+					{
+						for(int j=0;j<n;j++)
+						{
+							if(i+j<=n/2||i-j>=n/2||i==0||i==n-1)
+							{
+								System.out.print("*");				
+							}
+							else
+							{
+								System.out.print(" ");
+							}
+						}
+						{
+							System.out.println(" ");
+						}
+			}
+					
+		}
+
+
+	}
+
+		}
+		}
+	}
+
