96b75c760c787a4b0dc8c8b2372c3f22b2c3f37d
himanshu0988 <127866082+himanshu0988@users.noreply.github.com>
2023-04-22 16:22:52 +0530

    Add files via upload

---
+++ fourthquestion.java
@@ -0,0 +1,34 @@
+package firstproject;
+
+public class fourthquestion {
+
+	public static void main(String[] args) 
+	{
+		int n = 29;
+		{
+			{
+				for(int i=0;i<n;i++)
+				{
+					for(int j=0;j<n;j++)
+					{
+						if(i-j>=n/2||j+i>=n-1+(n/2))
+						{
+							System.out.print("*");				
+						}
+						else
+						{
+							System.out.print(" ");
+						}
+					}
+					{
+						System.out.println(" ");
+					}
+		}
+				
+	}
+
+
+	}
+
+}
+}
