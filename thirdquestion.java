846019e33dce537b979f5cbb472e638c3b5d6234
himanshu0988 <127866082+himanshu0988@users.noreply.github.com>
2023-04-22 16:15:00 +0530

    Add files via upload

---
+++ thirdquestion.java
@@ -0,0 +1,31 @@
+package firstproject;
+
+public class thirdquestion {
+
+	public static void main(String[] args) {
+		int n = 29;
+		{
+			{
+				for(int i=0;i<n;i++)
+				{
+					for(int j=0;j<n;j++)
+					{
+						if(i+j<=n/2||j-i>=n/2|| j==0 ||j==n-1||i==n-1)
+						{
+							System.out.print("*");				
+						}
+						else
+						{
+							System.out.print(" ");
+						}
+					}
+					{
+						System.out.println(" ");
+					}
+		}
+				
+	}
+
+}
+}
+}
